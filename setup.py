6ab72b41d2d896f94a3f188933a04e2c45c08105
rohithpr <praroh2@gmail.com>
2015-01-01 21:52:41 +0530

    Version error

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
  
 setup(
     name = 'py-web-search',
-    packages = ['py-web-search'],
-    version = '0.1.1.1',
+    packages = ['pws'],
+    version = '0.1.1.2',
     description = 'A Python module to fetch and parse results from different search engines.',
     author='Rohith PR',
     author_email='praroh2@gmail.com',
     url='https://github.com/rohithpr/py-web-search',
-    download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1.1',
+    download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1.1.2',
     keywords = ['web search', 'web scraping'],
     classifiers=['Programming Language :: Python',
     'Programming Language :: Python :: 3',
