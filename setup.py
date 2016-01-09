7ad272d56b656a9e4abf313f5340293a19fd646a
rohithpr <praroh2@gmail.com>
2016-01-09 10:34:38 +0530

    Release 0.2.4

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'py-web-search',
     packages = ['pws'],
-    version = '0.2.3',
+    version = '0.2.4',
     description = 'A Python module to fetch and parse results from different search engines.',
     author = 'Rohith PR',
     author_email = 'praroh2@gmail.com',
     url = 'https://github.com/rohithpr/py-web-search',
-    download_url = 'https://github.com/rohithpr/py-web-search/tarball/0.2.3',
+    download_url = 'https://github.com/rohithpr/py-web-search/tarball/0.2.4',
     keywords = ['web search', 'web scraping'],
     classifiers = [
     'Intended Audience :: Developers',
